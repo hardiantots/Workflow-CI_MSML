f78face5cbba9b3cec5f61f5041fd3a0760e51d8
Hardianto T.S <111510893+hardiantots@users.noreply.github.com>
2025-06-03 15:03:26 +0800

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -8,22 +8,27 @@ from sklearn.metrics import accuracy_score, classification_report
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_path", type=str, default="Bank_Personal_Loan_preprocessing.csv")
 args = parser.parse_args()
-data_path = args.data_path
 
-df = pd.read_csv(data_path)
-X = df.drop(columns=["Personal Loan"])
-y = df["Personal Loan"]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+def load_and_split_data(data, target_column, test_size=0.2, random_state=None):
+    df = pd.read_csv(data)
+    X = df.drop(columns=[target_column])
+    y = df[target_column]
+    return train_test_split(X, y, test_size=test_size, random_state=random_state)
+
+X_train, X_test, y_train, y_test = load_and_split_data(args.data_path, 'Personal Loan')
 
 mlflow.set_experiment('Bank_Personal_Loan_Experiment')
 mlflow.set_tracking_uri('http://127.0.0.1:5000')
 
 with mlflow.start_run():
     mlflow.sklearn.autolog()
+
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    mlflow.log_metric("accuracy", acc)
-    mlflow.log_text(classification_report(y_test, y_pred), "classification_report.txt")
+    accuracy = accuracy_score(y_test, y_pred)
+    report = classification_report(y_test, y_pred)
+
+    mlflow.log_metric('accuracy', accuracy)
+    mlflow.log_text(report, 'classification_report.txt')
